a40e0f33c273be0fc782cbe316b95524b8c79372
bambuss <114941604+lolife3@users.noreply.github.com>
2023-04-11 17:23:13 +0200

    Random Password Generator
    NOT SAFE! 
    uses random library
    
    for secure password use secrets library

---
+++ random_password_generator.py
@@ -0,0 +1,23 @@
+import random
+
+
+def main():
+    ls = [chr(i) for i in range(33, 123)]
+    
+    
+    while True:
+        while True:
+            user_input = input("Input password length (integer only): ")
+            if user_input.isdigit():
+                break
+    
+            
+        password = ""    
+        for i in range(int(user_input)):
+            password += str(random.choice(ls))
+            
+        print(password)
+    
+        
+if __name__ == "__main__":
+    main()
